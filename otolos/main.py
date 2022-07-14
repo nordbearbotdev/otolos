da0ea732686fa3ece2eeee87e150ac4047f130a5
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-07-14 16:28:57 +0300

    Update main.py

--- otolos/main.py
+++ otolos/main.py
@@ -2,3 +2,9 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5QtGui import *
+
+import os
+import sys
+
+# Создаем класс с всеми функциями браузера Otolos и его GUI
+class MainWindow(MainWindows)
