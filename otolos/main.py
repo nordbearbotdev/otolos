14b5e0c50c7c0897af8730f3a21a84b2dc9c86b2
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-07-14 16:33:21 +0300

    Update main.py

--- otolos/main.py
+++ otolos/main.py
@@ -8,3 +8,11 @@ import sys
 
 # Создаем класс с всеми функциями браузера Otolos и его GUI
 class MainWindow(MainWindows)
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+      
+        # В переменной Otolos хранится окно браузера
+        self.otolos = QWebEngineView()
+        self.otolos.setUrl(QUrl("здесь URL сайта разработчика"))
+        
+        self.otolos.urlChanged.connect()
